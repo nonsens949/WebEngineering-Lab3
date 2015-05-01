493f1de442d31e1b7d2ac7e53222d41d027b3a2b
Anja <anja.heim@gmx.at>
2015-05-01 12:58:57 +0200

    in applikation noch was ausgebessert, weil diese datei ja noch nicht zum richtigen Projekt gehört

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,7 +7,7 @@ import views.html.*;
 
 public class Application extends Controller {
 	
-	@play.db.jpa.Transactional
+	//@play.db.jpa.Transactional
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
