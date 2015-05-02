01cd18cd6afe7b21ebe2a32d771d1bd46781530a
Anja <anja.heim@gmx.at>
2015-05-02 12:34:55 +0200

    whole Project

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,14 +2,35 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import jeopardy.at.ac.tuwien.big.we15.lab2.api.*;
 import views.html.*;
 
 public class Application extends Controller {
 	
-	//@play.db.jpa.Transactional
-    public static Result index() {
+	//wenn framework aufgerufen wird soll login-seite erscheinen
+	@play.db.jpa.Transactional
+    public static Result login() {
         return ok(index.render("Your new application is ready."));
     }
+	
+	//von login seite werden daten weitergeleitet
+	@play.db.jpa.Transactional
+    public static Result authentication() {
+        return ok(index.render("Your new application is ready."));
+    }
+	
+	JeopardyFactory factory = new PlayQuizFactory(jsonFilePat);
+	JeopardyGame game = factory.createGame(user);
+
+	Player human = game.getHumanPlayer(); // get human player
+	human.getProfit(); // get current profit of player
+	human.getLatestProfitChange(); // get last profit change (for display)
+
+	game.chooseHumanQuestion(questionId); // choose next human question
+	game.answerHumanQuestion(answerIds); // answer current human questions
+	game.isRoundStart(); // check if we are at the beginning of a new round
+	game.isAnswerPending(); // check if the current question needs to be answered
+	game.isGameOver(); // check if game is over
+	game.getWinner(); // winner of round or null if no winner exists yet
 
 }
---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := """we-lab3-group82"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.11.1"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaCore,
+  javaJpa,
+  "org.hibernate" % "hibernate-entitymanager" % "4.3.1.Final",
+  "com.google.code.gson" % "gson" % "2.2"
+)
+
+ TwirlKeys.templateImports +=  "scala.collection._"
+
+ TwirlKeys.templateImports +=  "at.ac.tuwien.big.we15.lab2.api._"
